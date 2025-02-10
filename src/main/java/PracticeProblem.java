fdc11973954db1bff00244a0ecd1d5e64707626a
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-02-10 19:12:58 +0000

    Review 4 COMPLETE

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -12,33 +12,65 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String userInput = scanner.nextLine();
+		String modifiedinput  = "5" + userInput;
+		int finalnumber = Integer.parseInt(modifiedinput);
+		finalnumber += 5;
+		System.out.println(finalnumber);
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String userInput = scanner.nextLine();
+		String modifiedinput  = "4" + userInput + "3";
+		double finalnumber = Double.parseDouble(modifiedinput);
+		finalnumber += 3.4;
+		System.out.println(finalnumber);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input a boolean: ");
+		String userInput = scanner.nextLine();
+		boolean finalnumber = Boolean.parseBoolean(userInput);
+		boolean swap = !finalnumber;
+		System.out.println(swap);
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String userInput = scanner.nextLine();
+		String modifiedinput  = userInput + "3";
+		int finalnumber = Integer.parseInt(modifiedinput);
+		int number2 = finalnumber +2;
+		char letter = (char) number2;
+		System.out.println(letter);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input an integer: ");
+		String userInput = scanner.nextLine();
+		String modifiedinput  = userInput + "1";
+		int finalnumber = Integer.parseInt(modifiedinput);
+		int number2 = finalnumber /2;
+		double convertjndasd = (double) number2;
+		System.out.println(convertjndasd);
 	}
 
 	public static void q6() {
-		//Write question 6 code here
-		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String userInput = scanner.nextLine();
+		String modifiedinput  = "1" + userInput;
+		double doublevalue = Double.parseDouble(modifiedinput);
+		int intValue = (int) doublevalue;
+		System.out.println(intValue);
+
 	}
 
 }
